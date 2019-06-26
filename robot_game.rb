e358277d90845975653d34cfa86b0476b511eba0
Alex <azuev061981@gmail.com>
2019-06-26 17:02:53 +0300

    first commit

---
+++ robot_game.rb
@@ -0,0 +1,29 @@
+class RobotGame
+  initialize(x = 5, y = 6, f = NORTH)
+    @x = x
+    @y = y
+    @f = f
+
+  end
+
+  def place(x, y, f)
+    f = [NORTH, SOUTH, EAST, WEST]
+    
+  end
+
+  def move
+
+  end
+
+  def left
+
+  end
+
+  def right
+
+  end
+
+  def report
+
+  end
+end
